6673dfeaaa5f2561696f917d683ff6e819e66a6f
Djeisly Rodrigues <djeisly@yahoo.com.br>
2022-11-29 11:25:29 -0300

    Config. Conexão com o Banco

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
+const path = require('path');
+require('dotenv').config({path: __dirname+'/.env'})
 const express = require("express")
 const app = express()
+const mongoose = require("mongoose")
 
+mongoose.connect(process.env.DATABASE_MONGO),  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
+
+let db = mongoose.connection;
+
+db.on("error", console.log.bind(console, "connection error:"))
+db.once("open", function (){
+  console.log("conexão feita com sucesso.")
+})
 
 
 
