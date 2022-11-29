f70e8d20181d4fe734f142862f89b13cb55fee59
Djeisly Rodrigues <djeisly@yahoo.com.br>
2022-11-29 11:06:13 -0300

    Criando Schema

---
+++ src/models/mentorModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const mentorSchema = new mongoose.Schema(
+  {
+    id: mongoose.Schema.Types.ObjectId,
+
+    mentorName: {
+      type: String,
+      required: true,
+    },
+
+    expertise: {
+      type: String,
+      required: true,
+    },
+
+    available: {
+      type: Boolean,
+      required: true,
+    },
+
+    linkedin: {
+      type: String,
+      required: true,
+    },
+    
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("mentor", mentorSchema);
