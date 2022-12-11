7a847eead546779bb53484a3fbc620fa4443b6f2
Djeisly Rodrigues <djeisly@yahoo.com.br>
2022-12-11 10:50:30 -0300

    Add Swagger and config

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const path = require('path');
 require('dotenv').config({path: __dirname+'/.env'})
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('../swagger/swagger_output.json');
+
 const express = require("express");
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -10,22 +14,23 @@ const mentorRoutes = require("./routes/mentorRoutes")
 
 
 mongoose.connect(process.env.DATABASE_MONGO),  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 };
 
 let db = mongoose.connection;
 
 db.on("error", console.log.bind(console, "connection error:"))
 db.once("open", function (){
-  console.log("conexão feita com sucesso.")
-})
+  console.log("Banco conectado com sucesso!")
+}) 
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/mymentor", mentorRoutes)
+app.use('/minha-documentacao', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 
 module.exports = app
