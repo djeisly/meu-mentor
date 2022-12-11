a513ee1ca53a0c75272a3a09552b63494a86cce1
Djeisly Rodrigues <djeisly@yahoo.com.br>
2022-12-11 11:33:51 -0300

    Corrected data in database conection

--- src/database/dbConnect.js
+++ src/database/dbConnect.js
@@ -7,25 +7,11 @@ mongoose.connect(DATABASE_MONGO, {
   useUnifiedTopology: true
 })
 
-
 let db = mongoose.connection;
 
+db.on("error", console.log.bind(console, "connection error:"))
+db.once("open", function (){
+  console.log("Banco conectado com sucesso!")
+}) 
+
 module.exports = db;
-
-/*require('dotenv-safe').config()
-const DATABASE_MONGO = process.env.DATABASE_MONGO
-const mongoose = require ("mongoose");
-mogoose.set("strictQuery", flase);
-
-const conect = async () => {
-  try {
-      await mongoose.connect(DATABASE_MONGO, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      })
-      
-      console.log("Banco conectado!")
-  } catch (error) {
-    console.erro(error)
-  }
-}    */
