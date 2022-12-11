2c31facebd9ba769d16439baaac380b894e2c44e
Djeisly Rodrigues <djeisly@yahoo.com.br>
2022-12-11 11:05:07 -0300

    Add correct render.

--- src/database/dbConnect.js
+++ src/database/dbConnect.js
@@ -1,5 +1,6 @@
 const DATABASE_MONGO = process.env.DATABASE_MONGO
 const mongoose = require ("mongoose");
+mongoose.set('strictQuery', false);
 
 mongoose.connect(DATABASE_MONGO, {
   useNewUrlParser: true,
@@ -10,3 +11,21 @@ mongoose.connect(DATABASE_MONGO, {
 let db = mongoose.connection;
 
 module.exports = db;
+
+/*require('dotenv-safe').config()
+const DATABASE_MONGO = process.env.DATABASE_MONGO
+const mongoose = require ("mongoose");
+mogoose.set("strictQuery", flase);
+
+const conect = async () => {
+  try {
+      await mongoose.connect(DATABASE_MONGO, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+      
+      console.log("Banco conectado!")
+  } catch (error) {
+    console.erro(error)
+  }
+}    */
